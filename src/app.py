df13ed8e8b007daef3dd886d2aaa1a4ffdde1f3d
Quentin Bordelon <qgbordelon@gmail.com>
2026-09-12 01:22:32 -0500

    chore: adding structure

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask, render_template
 
-# Find the exact folder where this app.py file lives
 base_dir = os.path.abspath(os.path.dirname(__file__))
 template_dir = os.path.join(base_dir, 'templates')
 
-# Force Flask to use that explicit path for templates
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
